caabbe45f7df8169cb0f7426868b1a87a950fa5b
Alex <xllxlex@gmail.com>
2023-05-09 16:46:47 +0200

    Add some elements to the Work model and resolve a problem with adding user to a work model thrue a saving data from form

--- main/models.py
+++ main/models.py
@@ -1,16 +1,53 @@
 from django.db import models
 
+from users.models import CustomUser
+
 
 class Work(models.Model):
     date = models.CharField(
+        null=False,
         default='00-00-00',
         max_length=8,
+        verbose_name='Data',
     )
     timestart = models.CharField(
+        null=False,
         default='00:00',
         max_length=5,
+        verbose_name='Początek',
     )
     timefinish = models.CharField(
+        null=False,
         default='00:00',
         max_length=5,
+        verbose_name='Koniec',
+    )
+    work_object = models.CharField(
+        null=True,
+        max_length=100,
+        verbose_name='Miejsce pracy',
+    )
+    work_type = models.CharField(
+        null=True,
+        max_length=100,
+        verbose_name='Czynność',
+    )
+    coffee_food = models.IntegerField(
+        null=True,
+        verbose_name='Kawa/Posiłki',
+    )
+    fuel = models.IntegerField(
+        null=True,
+        verbose_name='Paliwo',
+    )
+    prepayment = models.IntegerField(
+        null=True,      
+        verbose_name='Zaliczka',
+    )
+    phone_costs = models.IntegerField(
+        null=True,
+        verbose_name='Telefon',
+    )
+    user = models.ManyToManyField(
+        CustomUser
     )
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Work
 from django.http import HttpResponse
 
 from users.models import CustomUser
@@ -12,7 +12,21 @@ def userWork(request, pk):
         date = request.POST.get('date')
         timestart = request.POST.get('timestart')
         timefinish = request.POST.get('timefinish')
+        timefinish = request.POST.get('work_object')
+        timefinish = request.POST.get('work_type')
+        timefinish = request.POST.get('coffee_food')
+        timefinish = request.POST.get('fuel')
+        timefinish = request.POST.get('prepayment')
+        timefinish = request.POST.get('phone_costs')
+        user = CustomUser.objects.get(id=pk)
+        work = Work.objects.create()
+        work.date = date
+        work.timestart = timestart
+        work.timefinish = timefinish
+        work.user.add(user)
+        work.save()
         print(date, timestart, timefinish)
+        # return redirect(request, 'home')
     user = CustomUser.objects.get(id=pk)
     context = {'user': user}
     return render(request, 'user_work.html', context)
