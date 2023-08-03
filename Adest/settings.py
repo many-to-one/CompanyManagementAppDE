d7ae354c958257200877fe427bcb1271cc4674cc
Alex <xllxlex@gmail.com>
2023-08-03 21:14:38 +0200

    edit

--- Adest/settings.py
+++ Adest/settings.py
@@ -118,10 +118,8 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': os.environ.get('NAME'),
-        'NAME': 'adest_db',
-        # 'USER': os.environ.get('USER'),
-        'USER': 'admin',
+        'NAME': os.environ.get('NAME'),
+        'USER': os.environ.get('USER'),
         'PASSWORD': os.environ.get('PASSWORD'),
         'HOST': os.environ.get('HOST'),
         'PORT': os.environ.get('PORT'),
