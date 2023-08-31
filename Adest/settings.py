25207422b1672c758d82cba47a9e0893a517224a
Alex <xllxlex@gmail.com>
2023-08-31 21:13:20 +0200

    hide secrets

--- Adest/settings.py
+++ Adest/settings.py
@@ -191,10 +191,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'xllxlex@gmail.com'
-EMAIL_HOST_PASSWORD = 'ksjjgnrtcflzqgok'
-# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 
